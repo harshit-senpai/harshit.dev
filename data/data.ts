71083fcf82a51229a2185a3eb3064779be7b8c9e
harshit Mishra <miananya2@gmail.com>
2024-07-06 00:48:53 +0530

    initial setup

---
+++ data/data.ts
@@ -0,0 +1,33 @@
+export const DATA = {
+  name: "Harshit Mishra",
+  initials: "HM",
+  location: "Chhattisgrah, India",
+  description:
+    "Product Builder, Problem Solver, Full Stack Dev, Otaku, Gamer, and a Music Lover.",
+  summary:
+    "I'm a product Builder and full stack developer currently based in Chhattisgarh, India. Pursuing my Undergrad in Computer Science, I'm passionate about building products, I'm also a big fan of open source softwares - That's why i buld everything in Public. I mainly write code in Typescript",
+  skills: [
+    "React",
+    "Next.js",
+    "TypeScript",
+    "Node.js",
+    "Postgress",
+    "Docker",
+    "TailwindCSS",
+    "ShadcnUI",
+  ],
+  contact: {
+    email: "miharshit2@gmail.com",
+    social: {
+        GitHub: {
+            url: "https://github.com/harshit-senpai",
+        },
+        Linkedin: {
+            url: "https://www.linkedin.com/in/harshit-senpai",
+        },
+        X: {
+            url: "https://www.x.com/harhsit-senpai",
+        }
+    }
+  }
+};
